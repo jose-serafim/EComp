e08239ea0563dd70a6e21a19dfa3bf39cd91a063
Dr-Specimen <jose.j.sarmento@me.com>
2020-12-27 16:23:17 +0000

    soc-knn works

--- system.mk
+++ system.mk
@@ -1,9 +1,22 @@
 # Directories
 ROOT_DIR := .
-REMOTE_ROOT_DIR ?=sandbox/ecomp_repo
 
-# Host
-HOST = local
+# Server info
+#HOST = local
+SERVER := geiger@geiger.local
+#PORT := -p 22
+RSYNC_REMOTE := -e 'ssh $(PORT)'
+SSH := ssh $(SERVER) $(PORT)
+
+REMOTE_ROOT_DIR ?=sandbox/$(USER)/ecomp_repo
+REMOTE_USER := geiger
+DOMAIN := $(REMOTE_USER).local
+#DOMAIN := $(USER).com
+#PORT := -p 22
+SERVER := $(REMOTE_USER)@$(DOMAIN)
+RSYNC_REMOTE := -e 'ssh $(PORT)'
+SSH := ssh $(SERVER) $(PORT)
+
 
 # Ojective (sim/fpga); default is sim
 OBJ = sim
